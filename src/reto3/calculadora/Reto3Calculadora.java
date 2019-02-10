1218cb7e8a9eb945c5559d96c2b8cd6374bd6782
aldocesarmg <aldocesarmg@gmail.com>
2019-02-09 18:17:53 -0600

    Creacion del archivo

---
+++ src/reto3/calculadora/Reto3Calculadora.java
@@ -0,0 +1,18 @@
+
+package reto3.calculadora;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Reto3Calculadora {
+
+    public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("¿Qué operación desea realizar?");
+        
+    }
+    
+}
