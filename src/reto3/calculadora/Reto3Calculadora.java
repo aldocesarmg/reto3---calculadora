320603c8c9b75a916d5bdd249287d53760ef1cae
aldocesarmg <aldocesarmg@gmail.com>
2019-02-09 18:56:06 -0600

    Operaciones y sus respectivos resultados

--- src/reto3/calculadora/Reto3Calculadora.java
+++ src/reto3/calculadora/Reto3Calculadora.java
@@ -11,26 +11,37 @@ public class Reto3Calculadora {
     
     public static void main(String[] args) throws IOException {
         String respuesta;
-        double valor1, valor2;
+        double valor1, valor2,resultado;
         System.out.println("¿Qué operación desea realizar?");
         respuesta = entrada.readLine();
+        System.out.println("Digita el primer número:");
+        valor1 = Double.parseDouble(entrada.readLine());
+        System.out.println("Digita el segundo número:");
+        valor2 = Double.parseDouble(entrada.readLine());
         
         switch(respuesta){
             case "Suma":
             case "suma":
-                
+                resultado = valor1+valor2;
+                System.out.println("El resultado de la operación "+valor1+" + "+valor2+" es igual a "+resultado);
                 break;
             case "Resta":
             case "resta":
-                
+                resultado = valor1-valor2;
+                System.out.println("El resultado de la operación "+valor1+" - "+valor2+" es igual a "+resultado);
                 break;
             case "Multiplicacion":
             case "multiplicacion":
-                
+                resultado = valor1*valor2;
+                System.out.println("El resultado de la operación "+valor1+" x "+valor2+" es igual a "+resultado);
                 break;
             case "Division":
             case "division":
-                
+                resultado = valor1/valor2;
+                System.out.println("El resultado de la operación "+valor1+" ÷ "+valor2+" es igual a "+resultado);
+                break;
+            default:
+                System.out.println("Ha habido un error");
                 break;
         }
     }
